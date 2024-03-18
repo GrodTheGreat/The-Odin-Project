35d931b2a60509cf6af538122063bf229ad167fb
GCausse <GCausse@epri.com>
2024-03-18 11:56:19 -0400

    Add random choice for computer

---
+++ PROJECT-rock-paper-scissors/main.js
@@ -0,0 +1,20 @@
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomChoice = Math.floor(Math.random() * 3);
+    const choice = choices[randomChoice];
+    console.log(choice);
+    return choice;
+}
+
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
+getComputerChoice();
