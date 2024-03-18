455bdc828c7fea99e7827f88e44b7fbf28aa61a9
GCausse <GCausse@epri.com>
2024-03-18 13:01:13 -0400

    Complete game loop

--- PROJECT-rock-paper-scissors/main.js
+++ PROJECT-rock-paper-scissors/main.js
@@ -7,10 +7,10 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    // your code here!
+    // First let's get some consistent strings
     const playerSelectionLower = playerSelection.toLowerCase();
     const playerSelectionProper = playerSelection[0].toUpperCase() + playerSelection.substr(1).toLowerCase();
-    const computerSelectionLower = computerSelection.toLowerCase() == 'paper';
+    const computerSelectionLower = computerSelection.toLowerCase();
     
     // Lose Message
     const loseMessage = `You Lose! ${playerSelectionProper} loses to ${computerSelection}`;
@@ -20,22 +20,53 @@ function playRound(playerSelection, computerSelection) {
     const tieMessage = `You Tied! ${playerSelectionProper} vs. ${computerSelection}`;
 
     if ((playerSelectionLower === 'rock') && (computerSelectionLower == 'paper')) {
-        return loseMessage; 
+        console.log(loseMessage); 
+        return 'lose';
     } else if ((playerSelectionLower === 'paper') && (computerSelectionLower == 'scissors')) {
-        return loseMessage; 
+        console.log(loseMessage); 
+        return 'lose';
     } else if ((playerSelectionLower === 'scissors') && (computerSelectionLower == 'rock')) {
-        return loseMessage;    
+        console.log(loseMessage);    
+        return 'lose';
     } else if ((playerSelectionLower === 'rock') && (computerSelectionLower == 'scissors')) {
-        return winMessage;
+        console.log(winMessage);
+        return 'win';
     } else if ((playerSelectionLower === 'paper') && (computerSelectionLower == 'rock')) {
-        return winMessage;
+        console.log(winMessage);
+        return 'win';
     } else if ((playerSelectionLower === 'scissors') && (computerSelectionLower == 'paper')) {
-        return winMessage;
+        console.log(winMessage);
+        return 'win';
     } else {
-        return tieMessage;
+        console.log(tieMessage);
+        return 'tie';
+    }
+}
+
+function playGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt('Rock, Paper, or Scissors?');
+        let result = playRound(playerChoice, getComputerChoice());
+        if (result == 'win') {
+            playerWins++;
+        } else if (result == 'lose') {
+            computerWins++;
+        } 
+    }
+
+    if (playerWins > computerWins) {
+        console.log(`Congratulations! You won ${playerWins} times!`);
+    } else if (computerWins > playerWins) {
+        console.log(`Bummer! You only won ${playerWins} times!`)
+    } else {
+        console.log(`Not bad! You tied!`)
     }
 }
   
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+playGame();
+// console.log(playRound(playerSelection, computerSelection));
