fa24ce08cbce20a990639918f147c8b5aded0ff8
Gerard Causse <grod.causse@gmail.com>
2024-03-22 23:10:42 -0400

    Add solution, tests may be broken, verified solutions in browser

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, numRepeats) {
+    if (numRepeats < 0) {
+        return 'ERROR';
+    }
+    
+    newText = '';
 
+    for (let i = 0; i < numRepeats; i++) {
+        newText += text;
+    }
+
+    return newText;
 };
 
 // Do not edit below this line
