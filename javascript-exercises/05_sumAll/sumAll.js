739fb3f59beedc5cfb6c36c8b61f0d6c962a9971
Gerard Causse <grod.causse@gmail.com>
2024-03-22 23:54:31 -0400

    Add solution, all tests passed, had to tes tin browser, test did not work locally

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
 const sumAll = function() {
+    let min;
+    let max;
 
+    if ((typeof(arguments[0]) != 'number') || (typeof(arguments[1]) != 'number')) {
+        return 'ERROR';
+    }
+
+    if ((arguments[0] < 0) || (arguments[0] < 0)) {
+        return 'ERROR';
+    }
+
+    if (arguments[0] > arguments[1]) {
+        min = arguments[1];
+        max = arguments[0];
+    } else {
+        min = arguments[0];
+        max = arguments[1];
+    }
+
+    let sum = 0;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
