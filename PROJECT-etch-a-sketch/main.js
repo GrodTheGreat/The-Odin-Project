bd8ff7f787adcdf6b00e9d0e09bbf7dc5cede951
GCausse <GCausse@epri.com>
2024-03-26 18:09:02 -0400

    Create grid on screen

---
+++ PROJECT-etch-a-sketch/main.js
@@ -0,0 +1,12 @@
+function createGrid() {
+    const container = document.querySelector('.container');
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid-square');
+            container.appendChild(gridSquare);
+        }
+    }
+}
+
+createGrid();
