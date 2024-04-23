2b04d5577f323bc2635fd81ad882671fc00e27b3
GCausse <GCausse@epri.com>
2024-04-23 17:25:27 -0400

    Complete exercise 9

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleaned_string = string.replace(/[.,!,'?\s]/g, '').toLowerCase();
+    for (let i = 0; i < cleaned_string.length; i++) {
+        if (cleaned_string[i] != cleaned_string[cleaned_string.length - (i + 1)]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
