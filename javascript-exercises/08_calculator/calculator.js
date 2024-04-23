e9c6d0d2cb0a951acdf77c6de9a424012c2b57d4
GCausse <GCausse@epri.com>
2024-04-23 15:55:26 -0400

    Complete exercise 8

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((sum, item) => {
+    return sum + item
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+	return nums.reduce((product, item) => {
+    return product * item
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
