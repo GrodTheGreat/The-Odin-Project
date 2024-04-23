4dad52ae3ac16fa47e79a0692c2fcd025bef3388
GCausse <GCausse@epri.com>
2024-04-23 18:56:51 -0400

    Complete exercise 12

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((max, item) => {
+        if (!max) return item;
+        const maxAge = getAge(max);
+        const age = getAge(item);
 
+        return (age > maxAge) ? item : max;
+    });
 };
+
+function getAge(person) {
+    if (!person.yearOfDeath) return new Date().getFullYear() - person.yearOfBirth;
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
