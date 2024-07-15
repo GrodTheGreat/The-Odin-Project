77afa5bb94ad0620648f5ed971c787c1385b13dd
GCausse <grod.causse@gmail.com>
2024-07-15 18:51:22 -0400

    Add game logic, still need to implement victory logic

--- projects/tic-tac-toe/main.js
+++ projects/tic-tac-toe/main.js
@@ -64,49 +64,114 @@ const tic = (() => {
         };
     };
 
-    // const Game = (() => {
-    //     let player1Turn = true;
-    //     let gameOver = false;
+    const Game = (() => {
+        let player1Turn = true;
+        let gameOver = false;
 
-    //     const player1 = createPlayer('Player 1', 'X');
-    //     const player2 = createPlayer('Player 2', 'O');
+        const player1 = createPlayer('Player 1', 'X');
+        const player2 = createPlayer('Player 2', 'O');
 
-    //     const gameboard = document.querySelector('.gameboard');
-    //     gameboard.addEventListener('click', (e) => {
-    //         const target = e.target;
-    //         console.log(target);
-    //     });
+        const gameboard = document.querySelector('.gameboard');
+        gameboard.addEventListener('click', (e) => {
+            const target = e.target;
+            console.log(target);
+        });
 
-    //     const checkWinner = () => {};
-    //     const checkRow = (row, marker) => {
-    //         if (typeof(row) !== Number) {
-    //             return false;
-    //         }
-    //         if (row > 2) {
-    //             return false;
-    //         }
-    //         if (row < 0) {
-    //             return false;
-    //         }
+        const checkWinner = () => {
+            
+        };
 
-    //         if (typeof(marker) !== String) {
-    //             return false;
-    //         }
-    //         if ((marker !== 'X') || (marker !== 'O')) {
-    //             return false;
-    //         }
+        const checkRow = (row, marker) => {
+            if (typeof(row) !== Number) {
+                return false;
+            }
+            if (row > 2) {
+                return false;
+            }
+            if (row < 0) {
+                return false;
+            }
 
-    //         for (i = 0; i < 2; i++) {
-    //             if (GameBoard.getGameboard()[row][i] !== marker) {
-    //                 return false;
-    //             }
-    //             else {
-    //                 return true;
-    //             }
-    //         }
-    //     };
-    //     const checkColumn = (column) => {};
-    // })();
+            if (typeof(marker) !== String) {
+                return false;
+            }
+            if ((marker !== 'X') || (marker !== 'O')) {
+                return false;
+            }
+
+            for (i = 0; i < 2; i++) {
+                if (GameBoard.getGameboard()[row][i] !== marker) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        };
+        const checkColumn = (column, marker) => {
+            if (typeof(column) !== Number) {
+                return false;
+            }
+            if (column > 2) {
+                return false;
+            }
+            if (column < 0) {
+                return false;
+            }
+
+            if (typeof(marker) !== String) {
+                return false;
+            }
+            if ((marker !== 'X') || (marker !== 'O')) {
+                return false;
+            }
+
+            for (i = 0; i < 2; i++) {
+                if (GameBoard.getGameboard()[i][column] !== marker) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        };
+
+        const checkLeftColumn = (marker) => {
+            if (typeof(marker) !== String) {
+                return false;
+            }
+            if ((marker !== 'X') || (marker !== 'O')) {
+                return false;
+            }
+
+            for (i = 0; i < 2; i++) {
+                if (GameBoard.getGameboard()[i][i] !== marker) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        };
+
+        const checkRightColumn = (marker) => {
+            if (typeof(marker) !== String) {
+                return false;
+            }
+            if ((marker !== 'X') || (marker !== 'O')) {
+                return false;
+            }
+
+            for (i = 2; i > 0; i--) {
+                if (GameBoard.getGameboard()[i][i] !== marker) {
+                    return false;
+                }
+                else {
+                    return true;
+                }
+            }
+        };
+    })();
 
     const events = (() => {
         const events = {};
@@ -140,6 +205,33 @@ const tic = (() => {
         }
     })();
 
+
+    const DisplayController = (() => {
+        const game = document.querySelector('.game');
+        
+        const player1Name = game.querySelector('#player-1-score').firstChild.textContent;
+        const player1Score = game.querySelector('#player-1-score').lastChild.textContent;
+        
+        const player2Name = game.querySelector('#player-2-score').firstChild.textContent;
+        const player2Score = game.querySelector('#player-2-score').lastChild.textContent;
+
+
+        const form = document.querySelector('form');
+        const player1 = form.querySelector('#player-1');
+        const player2 = form.querySelector('#player-2');
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            console.log('Submitted');
+        });
+
+
+        return {game, form, player1, player2, player1Name, player1Score};
+    })()
+
     return {
+        DisplayController
     };
+
+
 })();
